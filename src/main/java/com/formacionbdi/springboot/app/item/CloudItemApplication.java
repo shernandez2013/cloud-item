f0602279f13ce77ba990424522a2cc08ee25c624
shernandez <silvano.hernandez@citi.com.mx>
2021-06-20 17:56:58 -0500

    Primer commit de codigo

---
+++ src/main/java/com/formacionbdi/springboot/app/item/CloudItemApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudItemApplication.class, args);
+	}
+
+}
